a42f860840b7173a23c7cce16c6fac3ab3e0accd
malyfko <malyfko@gmail.com>
2018-01-07 05:55:48 +0100

    Different abilities of different fields

--- src/scripts/Cell.js
+++ src/scripts/Cell.js
@@ -1,13 +1,15 @@
 class Cell {
 
-  constructor () {
+  constructor (correspondingShip, attempted, cellAttempt, fieldType, activeField) {
     this.cell = document.createElement('div');
-    this.attempted = false;
-    this.correspondingShip = null;
+    this.attempted = attempted;
+    this.correspondingShip = correspondingShip;
 
     this.cell.className = 'cell';
     this.cell.append(String.fromCharCode(0x000A0));
-    this.cell.addEventListener('click', this.attemptCell.bind(this));
+    if (fieldType === 'computer' && activeField) {
+      this.cell.addEventListener('click', this.attemptCell.bind(this));
+    }
   }
 
   attemptCell () {
