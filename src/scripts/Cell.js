6e5e034d98456579f56c5074c4a6885ae658b943
malyfko <malyfko@gmail.com>
2018-01-07 04:48:03 +0100

    Add onClick handler for cell

--- src/scripts/Cell.js
+++ src/scripts/Cell.js
@@ -1,11 +1,20 @@
-const Cell = () => {
+class Cell {
 
-  const cell = document.createElement('div');
-  cell.className = 'cell';
+  constructor () {
+    this.cell = document.createElement('div');
+    this.attempted = false;
+    this.correspondingShip = null;
 
-  return (
-    cell
-  );
-};
+    this.cell.className = 'cell';
+    this.cell.append(String.fromCharCode(0x000A0));
+    this.cell.addEventListener('click', this.attemptCell.bind(this));
+  }
+
+  attemptCell () {
+    this.attempted = true;
+    this.cell.innerHTML = this.correspondingShip ? 'X' : String.fromCharCode(0x000B7);
+  }
+
+}
 
 export default Cell;
